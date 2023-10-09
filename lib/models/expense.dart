a9ff1b7925f4874a0cef19cc43b9ec819c6ce2e8
HenryBS18 <110516086+HenryBS18@users.noreply.github.com>
2023-10-09 16:27:44 +0800

    change file name and add new field 'id'

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,8 +1,9 @@
 class Expense {
+  int id;
   int amount;
   String category;
   String? notes;
   DateTime date;
 
-  Expense(this.amount, this.category, this.notes, this.date);
+  Expense(this.id, this.amount, this.category, this.notes, this.date);
 }
